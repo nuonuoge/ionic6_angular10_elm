84f0c368afa91051761510d66a715eb47e7c9151
gury <zbrongyaozb@163.com>
2020-08-31 09:27:14 +0800

    gury

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,6 +12,8 @@ export class HomePage implements OnInit {
   guessCityId: string;
   hotCities: any[];
   groupCities: any;
+  hotcity = 'hotcity';
+
   constructor(public dataService: DataService) {
     this.guessCity = '';
     this.hotCities = [];
@@ -32,16 +34,25 @@ export class HomePage implements OnInit {
       this.guessCityId = '';
     });
   }
-
   getHotCity() {
     this.dataService.getHotCity().subscribe(res => {
+      try {
+        this.hotcity = JSON.stringify(res);
+      } catch (error) {
+        this.hotcity = 'json err hotcity';
+      }
       if (Array.isArray(res)) {
         this.hotCities = res.map((city) => {
-          return { name: city.name, id: city.id };
+          return { name: city.name , id: city.id };
         });
       }
     }, err => {
       this.hotCities = [];
+      try {
+        this.hotcity = JSON.stringify(err);
+      } catch (error) {
+        this.hotcity = 'jsonerr err hotcity';
+      }
     });
   }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
   production: false,
-  apiUrl: ''
+  apiUrl: 'http://cangdu.org:8001'
 };
 
 /*
